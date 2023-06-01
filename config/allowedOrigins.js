1748ff9d00f25209b4f4d85c10039f478470e9bd
ichie chinemrem richard <chinemeremichie@gmail.com>
2023-06-01 10:58:08 +0100

    first commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,5 @@
+const allowedOrigins = [
+    'https://Avvic-bank-coming-soon-page.onrender.com'
+]
+
+module.exports=allowedOrigins
---
+++ index.js
@@ -0,0 +1,52 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors')
+require('dotenv').config()
+
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+
+// app.post("/post_name",async(req,res)=>{
+//   let {name} = req.body
+//   console.log(name)
+  
+// })
+app.post('/send-email', async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.USER,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: 'chinemeremiche@gmail.com',
+      to: 'chinemeremichie@gmail.com',
+      subject: 'New email from the website',
+      text: `Email: ${email}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${[PORT]}`);
+});
